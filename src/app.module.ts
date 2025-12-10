d6e0ea6bb3b38aaa48a00b3dffd4d735ed0091aa
Maschenko Dmitry <tina8069@yandex.ru>
2025-12-10 23:21:59 +0300

    imported all modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,19 @@
 import { Module } from '@nestjs/common';
+import { PlayerModule } from './player/player.module';
+import { RmqConfigService } from './rmq-config/rmq-config.service';
+import { RmqConfigModule } from './rmq-config/rmq-config.module';
+import { ConfigModule } from '@nestjs/config';
+import { InteractionService } from './interaction/interaction.service';
+import { InteractionModule } from './interaction/interaction.module';
+import { DiscordConfigModule } from './discord-config/discord-config.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    InteractionModule,
+    PlayerModule,
+    RmqConfigModule,
+    DiscordConfigModule,
+  ],
 })
 export class AppModule {}
