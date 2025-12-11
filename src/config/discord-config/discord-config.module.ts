10a9140ff762dcd4359eb2aff453da12b6f5333a
Дмитрий Мащенко <d.maschenko@miac29.ru>
2025-12-11 11:53:07 +0300

    moved configs to separate dir

--- src/config/discord-config/discord-config.module.ts
+++ src/config/discord-config/discord-config.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { DiscordConfigService } from './discord-config.service';
 
+@Global()
 @Module({
   providers: [DiscordConfigService],
   exports: [DiscordConfigService],
--- src/config/rmq-config/rmq-config.module.ts
+++ src/config/rmq-config/rmq-config.module.ts
@@ -1,5 +1,4 @@
 import { Global, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { RmqConfigService } from './rmq-config.service';
 
 @Global()
